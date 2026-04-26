055d3ffd9041c64fe696a6b610e7f85af199e628
20183 <2018372151@qq.com>
2026-04-26 23:16:19 +0800

    更新

--- Backend/flowly_backend/settings/cors.py
+++ Backend/flowly_backend/settings/cors.py
@@ -6,7 +6,7 @@ from .security import DEBUG
 
 CORS_ALLOWED_ORIGINS = os.getenv(
     "CORS_ALLOWED_ORIGINS",
-    "http://localhost:5173,http://127.0.0.1:5173,http://localhost:5174,http://127.0.0.1:5174",
+    "http://localhost:5173,http://127.0.0.1:5173,http://localhost:5174,http://127.0.0.1:5174,https://ming3-l.github.io",
 ).split(",")
 
 CORS_ALLOW_ALL_ORIGINS = DEBUG
